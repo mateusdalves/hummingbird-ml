995735b0172ac7bfbcc68ff43a12860d439cb5ab
Mateus <alves.mateus@protonmail.com>
2021-11-19 21:06:00 -0300

    adicionado projeto ao repositório

---
+++ tensorb.bat
@@ -0,0 +1,3 @@
+conda activate ml-agents-1
+
+tensorboard --logdir ./config/results --port 6006
---
+++ train.bat
@@ -0,0 +1,3 @@
+conda activate ml-agents-1
+
+mlagents-learn ./config/trainer_config.yaml --run-id hb_01 
